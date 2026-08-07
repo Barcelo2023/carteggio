49c616a68cf86418af4e93cc3d87179046670f58
Barcelo2023 <ibradicich@virgilio.it>
2026-08-07 11:28:28 +0200

    Add files via upload

--- carico.pubbl.js
+++ carico.pubbl.js
@@ -3,8 +3,9 @@ window.addEventListener('DOMContentLoaded', () => {
     // Inietta lo script principale di Google AdSense valido sia per PC che per Telefono
     const scriptAdSense = document.createElement('script');
     scriptAdSense.async = true;
-    scriptAdSense.src = "https://googlesyndication.com";
-    scriptAdSense.crossOrigin = "anonymous";
+    // Assegna l'URL corretto alla proprietà src
+    scriptAdSense.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7064918153021324";
+    scriptAdSense.crossOrigin = "anonymous";    
     document.head.appendChild(scriptAdSense);
     
     console.log("Google AdSense attivato globalmente per il sito web.");
