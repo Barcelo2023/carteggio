2f4fda403db1931cbd64d4dd4819894781925d05
Barcelo2023 <ibradicich@virgilio.it>
2026-08-09 14:14:04 +0200

    Add files via upload

--- dataora.js
+++ dataora.js
@@ -8,6 +8,6 @@
      const ore = String(data.getHours()).padStart(2, '0');
      const minuti = String(data.getMinutes()).padStart(2, '0');
      
-      document.getElementById('datetime').textContent =  `${anno}/${mese}/${giorno}    ${ore}:${minuti}`;
+      document.getElementById('datetime').textContent =  `${anno}.${mese}.${giorno}    ${ore}:${minuti}`;
 
         }
