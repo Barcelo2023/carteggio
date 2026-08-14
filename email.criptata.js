629ec893d977147485d58cca408fedb222ab3b56
Barcelo2023 <ibradicich@virgilio.it>
2026-08-14 15:05:26 +0200

    Add files via upload

---
+++ email.criptata.js
@@ -0,0 +1,17 @@
+
+ async function hashEmail(email) {
+  const emailPulita = String(email).replace(/\s+/g, '').toLowerCase();
+  // 1. Converte il testo dell'email in un array di byte (Uint8Array)
+  const msgBuffer = new TextEncoder().encode(emailPulita);                    
+
+  // 2. Genera l'hash SHA-256 (restituisce un ArrayBuffer)
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+  // 3. Converte l'ArrayBuffer in una stringa esadecimale leggibile
+  const hashArray = Array.from(new Uint8Array(hashBuffer));                     
+  const chiave = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+   
+  return chiave;
+}
+
+
