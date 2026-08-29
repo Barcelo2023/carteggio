155e8fb600fffcbdb7bd4bdfedf7846d9d983092
Barcelo2023 <ibradicich@virgilio.it>
2026-08-29 21:46:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 self.options = {
-
     "domain" : "3nbf4.com",
     "zoneID" : 11681245
 }
 self.lary = ""
-imbpoertScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
